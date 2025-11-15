5286ab023a8e8b5604a9eb27c3a8a079bc6ddac3
AleDeb93 <alex.debiasi@email.com>
2025-11-15 16:12:59 +0100

    Client Login and Registration

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,25 +1,38 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { lastValueFrom } from 'rxjs';
+import { Nav } from "../layout/nav/nav";
+import { AccountService } from '../core/services/account-service';
+import { Home } from "../components/home/home";
+import { User } from '../types/user';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Nav, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App implements OnInit {
+  private accountService = inject(AccountService)
   private http = inject(HttpClient)
   protected readonly title = signal('Heart Beat');
-  protected users = signal<any>([]);
+  protected users = signal<User[]>([]);
 
   async ngOnInit(): Promise<void> {
-    this.users.set(await this.getUsers())
+    this.users.set(await this.getUsers());
+    this.setCurrentUser();
+  }
+
+  setCurrentUser(){
+    const userString = localStorage.getItem('user');
+    if(!userString) return;
+    const user = JSON.parse(userString);
+    this.accountService.currentUser.set(user);
   }
 
   async getUsers() {
     try{
-      return lastValueFrom(this.http.get('https://localhost:7289/api/users')); 
+      return lastValueFrom(this.http.get<User[]>('http://localhost:5285/api/users')); 
     } catch (error){
       console.log(error);
       throw error;
